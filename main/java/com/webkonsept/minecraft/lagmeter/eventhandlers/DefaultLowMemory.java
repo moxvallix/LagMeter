5da294ed7fd25471e13dd09ff136385937d5f800
Dan <theexplosivefrog@aim.com>
2013-05-14 19:13:53 -0400

    Wrong method.

--- main/java/com/webkonsept/minecraft/lagmeter/eventhandlers/DefaultLowMemory.java
+++ main/java/com/webkonsept/minecraft/lagmeter/eventhandlers/DefaultLowMemory.java
@@ -24,7 +24,7 @@ public class DefaultLowMemory implements MemoryListener{
 				p.sendMessage(ChatColor.GOLD+"[LagMeter] "+ChatColor.RED+"The server's free memory pool has dropped below "+this.plugin.getMemoryNotificationThreshold()+"%! If you configured a server command to execute at this time, it will run now.");
 		this.plugin.severe("The server's free memory pool has dropped below "+this.plugin.getMemoryNotificationThreshold()+"%! Executing command (if configured).");
 		if(this.plugin.getLagCommand().contains(";"))
-			for(final String cmd: this.plugin.getLagCommand().split(";"))
+			for(final String cmd: this.plugin.getMemoryCommand().split(";"))
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), cmd.replaceFirst("/", ""));
 		else
 			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), this.plugin.getLagCommand().replaceFirst("/", ""));
