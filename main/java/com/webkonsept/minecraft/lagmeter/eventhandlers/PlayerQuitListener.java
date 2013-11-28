e503e0139b2344c9cfabe5faaf8d8164816e228d
Dan <theexplosivefrog@aim.com>
2013-11-27 22:35:46 -0500

    Probably shouldn't have players in this list while they're offline, or they'll never be able to return to their old map(?)

--- main/java/com/webkonsept/minecraft/lagmeter/eventhandlers/PlayerQuitListener.java
+++ main/java/com/webkonsept/minecraft/lagmeter/eventhandlers/PlayerQuitListener.java
@@ -1,6 +1,7 @@
 package com.webkonsept.minecraft.lagmeter.eventhandlers;
 
 import com.webkonsept.minecraft.lagmeter.LagMeter;
+import com.webkonsept.minecraft.lagmeter.exceptions.NoActiveLagMapException;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,5 +11,10 @@ public class PlayerQuitListener implements Listener{
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerQuitEvent(PlayerQuitEvent evt){
 		LagMeter.getInstance().removePlayerIP(evt.getPlayer().getName());
+		try{
+			LagMeter.getInstance().turnLagMapOff(evt.getPlayer());
+		}catch(NoActiveLagMapException e){
+			//do nothing
+		}
 	}
 }
